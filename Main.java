72ddd6092e0804fe9521ceaf7895e03187b85cb5
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2025-05-11 23:17:42 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.awt.*;
 import java.util.List;
 
 import java.util.Scanner;
@@ -14,7 +15,9 @@ public class Main {
             System.out.println("2. Remove Asset");
             System.out.println("3. View Assets");
             System.out.println("4. View Portfolio Summary");
-            System.out.println("5. Exit");
+            System.out.println("5. Calculate Zakat");
+            System.out.println("6. Connect Card");
+            System.out.println("7. Exit");
             System.out.print("Choose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -57,26 +60,26 @@ public class Main {
 
                     Asset newAsset = new Asset(assetID, assetName, assetType, purchasePrice, quantity, currentPrice);
                     manager.addAsset(newAsset);
-                    break;
+                    return;
 
                 case 2:
                     System.out.print("Enter the ID of the asset to remove: ");
                     int removeID = scanner.nextInt();
                     scanner.nextLine();
                     manager.removeAsset(removeID);
-                    break;
+                    return;
 
                 case 3:
-                    List<String> assetNames = manager.getAssets();
+                    List<Asset> assetNames = manager.getAssets();
                     if (assetNames.isEmpty()) {
                         System.out.println("No assets available.");
                     } else {
                         System.out.println("Assets:");
-                        for (String assetNameStr : assetNames) {
-                            System.out.println("- " + assetNameStr);
+                        for (Asset assetNameStr : assetNames) {
+                            System.out.println("- " + assetNameStr.getAssetName());
                         }
                     }
-                    break;
+                    return;
 
                 case 4:
                     if (manager.getAssets().isEmpty()) {
@@ -84,10 +87,53 @@ public class Main {
                     } else {
                         manager.printPortfolioSummary();
                     }
-                    break;
-
+                    return;
 
                 case 5:
+                    System.out.println("Enter Asset Name:\n");
+                    String name = scanner.nextLine();
+                    boolean found = false;
+                    Asset A = null;
+                    List<Asset> Assets = manager.getAssets();
+                    for(Asset asset: Assets) {
+                        if(asset.getAssetName().equals(name)) {
+                            A = asset;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if(!found) {
+                        System.out.println("Asset not found.");
+                        return;
+                    }
+                    int duration = scanner.nextInt();
+                    while(duration < 0){
+                        System.out.println("Please inter a positive number\n");
+                        duration = scanner.nextInt();
+                    }
+                    ZakatCalculator zakat = new ZakatCalculator(A, duration);
+                    zakat.Calulate();
+                    System.out.println(zakat.getZakat());
+                    return;
+
+                case 6:
+                    System.out.println("Enter CardId:");
+                    int cardID = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Enter your Password:");
+                    String password = scanner.nextLine();
+                    Card card = new Card(cardID, password);
+                    if(!card.IsFound(card)){
+                        System.out.println("Card is not found\n");
+                        return;
+                    }
+                    BankAccount account = new BankAccount();
+                    System.out.print("Enter OTP Code: ");
+                    int code = scanner.nextInt();
+                    account.ConnectAccount(manager, card, code);
+                    return;
+
+                case 7:
                     System.out.println("Exiting...");
                     return;
 
@@ -97,4 +143,3 @@ public class Main {
         }
     }
 }
-
