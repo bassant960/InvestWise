6fdb7252671f08bef49f010edfa544d53ea40b18
fatmaabdelfatah <162179396+fatmaabdelfatah@users.noreply.github.com>
2025-05-11 16:07:55 +0300

    Update Asset.java

--- Asset.java
+++ Asset.java
@@ -1,43 +1,95 @@
 package org.example;
+
 /**
- * Represents an asset with a specific type and associated fields.
+ * Represents an investment asset with details such as ID, name, type,
+ * purchase price, quantity, and current market price.
+ * Provides methods to calculate current value and return on investment (ROI).
+ *
+ * @author
+ * @version 1.0
  */
 public class Asset {
-    private String type;
-    private String[] fields;
-    /**
-     * Constructs an Asset with the given type and fields.
-     *
-     * @param type   The type of the asset (e.g., "Stock", "RealEstate", "Gold","Crypto").
-     * @param fields The specific fields related to the asset.
-     */
+    private int assetID;
+    private String assetName;
+    private String assetType;
+    private float purchasePrice;
+    private float quantity;
+    private float currentMarketPrice;
 
-    public Asset(String type, String[] fields) {
-        this.type = type;
-        this.fields = fields;
-    }
     /**
-     * Returns the type of the asset.
+     * Constructs an Asset with the specified details.
+     *
+     * @param assetID             The unique identifier for the asset.
+     * @param assetName           The name of the asset.
+     * @param assetType           The type/category of the asset (e.g., stock, bond).
+     * @param purchasePrice       The price at which the asset was purchased.
+     * @param quantity            The amount/number of units owned.
+     * @param currentMarketPrice  The current market price of the asset.
+     */
+    public Asset(int assetID, String assetName, String assetType,
+                 float purchasePrice, float quantity, float currentMarketPrice) {
+        this.assetID = assetID;
+        this.assetName = assetName;
+        this.assetType = assetType;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.currentMarketPrice = currentMarketPrice;
+    }
+
+    /**
+     * Calculates the current total value of the asset.
+     *
+     * @return The current value (quantity × current market price).
+     */
+    public float calculateCurrentValue() {
+        return quantity * currentMarketPrice;
+    }
+
+    /**
+     * Calculates the Return on Investment (ROI) for the asset.
+     *
+     * @return The ROI percentage.
+     */
+    public float calculateROI() {
+        float initialInvestment = quantity * purchasePrice;
+        float currentValue = calculateCurrentValue();
+        return ((currentValue - initialInvestment) / initialInvestment) * 100;
+    }
+
+    /**
+     * Gets the unique ID of the asset.
+     *
+     * @return The asset ID.
+     */
+    public int getAssetID() {
+        return assetID;
+    }
+
+    /**
+     * Gets the name of the asset.
+     *
+     * @return The asset name.
+     */
+    public String getAssetName() {
+        return assetName;
+    }
+
+    /**
+     * Gets the type/category of the asset.
      *
      * @return The asset type.
      */
-    public String getType() {
-        return type;
+    public String getAssetType() {
+        return assetType;
     }
+
     /**
-     * Returns the fields associated with the asset.
+     * Returns a string representation of the asset.
      *
-     * @return An array of fields for this asset.
+     * @return A string containing asset name, type, and quantity.
      */
-    public String[] getFields() {
-        return fields;
-    }
-    /**
-     * Sets the fields for this asset.
-     *
-     * @param fields An array of fields to associate with the asset.
-     */
-    public void setFields(String[] fields) {
-        this.fields = fields;
+    @Override
+    public String toString() {
+        return assetName + " | Type: " + assetType + " | Quantity: " + quantity;
     }
 }
