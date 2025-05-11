d73cc0f6880c74ececf0ec925a2319cd3a914984
bassant960 <163014114+bassant960@users.noreply.github.com>
2025-05-12 02:12:37 +0300

    organize

---
+++ login-up/Menu.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+    public static void opensecondmenu(){}
+    public static void usermenu(){
+        String name ;
+        String pass ;
+        String email ;
+        String username;
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Sign In\n2. Sign Up\n3. Exit\nEnter your choice");
+        int choice;
+        choice = input.nextInt();
+        if(choice == 1){
+            System.out.print("enter your Username: ");
+            username = input.next();
+            System.out.print("enter your Password: ");
+            pass = input.next();
+            boolean success = User.signin(username, pass);
+            if(success){
+                opensecondmenu();
+                break;
+            }
+        }else if(choice == 2){
+            System.out.print("enter your name: ");
+            name = input.next();
+            System.out.print("enter your Username: ");
+            username = input.next();
+            System.out.print("enter your Password: ");
+            pass = input.next();
+            System.out.print("enter your Email: ");
+            email = input.next();
+            User user = new User(name, username, pass, email);
+            boolean success = user.signup();
+            if(success){
+                opensecondmenu();
+                break;
+            }
+
+        }else if(choice == 3){
+            System.out.print("Exiting.................. ");
+            System.exit(0);
+    }else{
+            System.out.println("Invalid choice try again");
+        }
+
+}
+    
+    public static void main(String[] args) throws IOException {
+
+        while(true) {
+            usermenu();
+        }
+    }
+    }
