a2cbf11e4e4d7d0fb7503b6b4a2c3b5c3c785cff
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2025-05-12 23:40:21 +0300

    Update connectCard.java

--- src/main/java/org/example/connectCard.java
+++ src/main/java/org/example/connectCard.java
@@ -10,56 +10,65 @@ import java.util.List;
 
 import com.google.gson.GsonBuilder;
 
-class Card{
+/**
+ * Represents a Card with CVV, CardNumber, and PIN details.
+ */
+class Card {
     private int CVV;
     private String CardNumber;
     private int PIN;
-    public Card(int cvv, String num, int pin){
+
+    /**
+     * Constructs a new Card with the given details.
+     *
+     * @param cvv The CVV of the card.
+     * @param num The card number.
+     * @param pin The card PIN.
+     */
+    public Card(int cvv, String num, int pin) {
         this.CVV = cvv;
         this.CardNumber = num;
         this.PIN = pin;
     }
 }
 
-class BankAccount{
-
+/**
+ * Represents a BankAccount that manages card connections and OTP verification.
+ */
+class BankAccount {
     OTP otp;
-    public BankAccount(){
+
+    /**
+     * Constructs a new BankAccount with an OTP instance.
+     */
+    public BankAccount() {
         this.otp = new OTP();
     }
-    public void ConnectAccount(PortfolioManager manager, Card card, int code){
-        if(!otp.VerifyOTP(code)){
+
+    /**
+     * Connects a card to a PortfolioManager if the provided OTP is valid.
+     *
+     * @param manager The PortfolioManager to connect the card to.
+     * @param card    The card to be connected.
+     * @param code    The OTP code provided for verification.
+     */
+    public void ConnectAccount(PortfolioManager manager, Card card, int code) {
+        if (!otp.VerifyOTP(code)) {
             return;
         }
         manager.connectcard(card);
         manager.saveCardsToFile();
         System.out.println("Card connected to Bank");
-
     }
 }
 
-class OTP{
+/**
+ * Represents an OTP system for generating and verifying one-time passwords.
+ */
+class OTP {
     int otpcode;
     Calendar calendar;
     int expairyDate;
-    public OTP() {
-        Random random = new Random();
-        this.otpcode = random.nextInt(100000);
-        this.expairyDate = 30;
-        calendar = Calendar.getInstance();
-        System.out.println("OTP code is " + this.otpcode);
-    }
-    public boolean VerifyOTP(int code){
-        calendar.add(Calendar.SECOND, expairyDate);
-        Calendar now = Calendar.getInstance();
-        if(now.after(calendar)){
-            System.out.println("Time expired\n");
-            return false;
-        }
-        if(code!=this.otpcode){
-            System.out.println("Wrong code\n");
-        }
-        return (code==this.otpcode);
-    }
-}
 
+    /**
+     * Constructs a new OTP with*
