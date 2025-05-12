ac32e7c1b16a1cb9ebb009fc136602752d48e3ce
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2025-05-12 23:39:32 +0300

    Update zakatcalculator.java

--- src/main/java/org/example/zakatcalculator.java
+++ src/main/java/org/example/zakatcalculator.java
@@ -1,33 +1,47 @@
 package org.example;
+
 import java.util.Scanner;
 
-
 /**
- * Represents an Asset's zakat calculator with details such as asset, Duration.
- * Provides methods to calculate Zakat depends on assetvalue, duration, gold price.
+ * Represents a Zakat calculator for assets.
+ * Handles Zakat calculation based on asset value, duration, and gold price.
  */
 public class ZakatCalculator {
-    Scanner scanner = new Scanner(System.in);
     private Asset asset;
     private int Duration;
     private double Zakat;
     private double Limit;
     final double GOLD_PRICE = 3715;
+
+    /**
+     * Constructs a new ZakatCalculator with the given asset and duration.
+     *
+     * @param asset    The asset for which Zakat is being calculated.
+     * @param Duration The ownership duration in years.
+     */
     public ZakatCalculator(Asset asset, int Duration) {
         this.asset = asset;
         this.Duration = Duration;
-        this.Limit = GOLD_PRICE*85;
+        this.Limit = GOLD_PRICE * 85;
     }
+
+    /**
+     * Returns the calculated Zakat value.
+     *
+     * @return The calculated Zakat value.
+     */
     public double getZakat() {
         return Zakat;
     }
-    public void Calulate(){
-        if(Duration >= 1 && asset.calculateCurrentValue()>=Limit){
-            this.Zakat = asset.calculateCurrentValue()*(0.025);
-        }
-        else{
+
+    /**
+     * Calculates the Zakat for the asset.
+     */
+    public void Calulate() {
+        if (Duration >= 1 && asset.calculateCurrentValue() >= Limit) {
+            this.Zakat = asset.calculateCurrentValue() * 0.025;
+        } else {
             System.out.println("This Asset hasn't zakat");
         }
     }
-
 }
